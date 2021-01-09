6d4aefd63b1b2b53cd468ad0c5481d31311f019a
容易学 <75765518+rystudy@users.noreply.github.com>
2021-01-09 18:33:00 +0800

    Update frps_linux_install.sh

--- frps_linux_install.sh
+++ frps_linux_install.sh
@@ -5,21 +5,21 @@ echo "最新版本：$FRP_VERSION"
 REPO="rystudy/frps"
 WORK_PATH=$(dirname $(readlink -f $0))
 
-# 创建frp文件夹
+echo "创建frp文件夹"
 mkdir -p /usr/local/frp && \
-# 下载并移动frps文件
+echo "下载并移动frps文件"
 wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 tar -zxvf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 cd frp_${FRP_VERSION}_linux_amd64 && \
 mv frps /usr/local/frp && \
-# 下载frps.in和frps.service
+echo "下载frps.in和frps.service"
 wget -P /usr/local/frp https://raw.githubusercontent.com/${REPO}/master/frps.ini && \
 wget -P /lib/systemd/system https://raw.githubusercontent.com/${REPO}/master/frps.service && \
 systemctl daemon-reload && \
-# 启动frps
+echo "启动frps"
 sudo systemctl start frps && \
 sudo systemctl enable frps && \
-# 删除多余文件
+echo "删除多余文件"
 cd ${WORK_PATH} && \
 rm -rf frp_${FRP_VERSION}_linux_amd64 frp_${FRP_VERSION}_linux_amd64.tar.gz frps_linux_install.sh
 echo "=======================================================" &&\
