bd3e0947bb4bbf82af115cda73f43be808ac069a
容易学 <75765518+rystudy@users.noreply.github.com>
2021-01-09 18:36:14 +0800

    Update frps_linux_install.sh

--- frps_linux_install.sh
+++ frps_linux_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FRP_VERSION=`curl -Ls -o /dev/null -w %{url_effective} https://github.com/fatedier/frp/releases/latest)`
+FRP_VERSION=`curl -Ls -o /dev/null -w %{url_effective} https://github.com/fatedier/frp/releases/latest`
 FRP_VERSION=`echo $FRP_VERSION | grep -Eo '[0-9]+.*'`
 echo "最新版本：$FRP_VERSION"
 REPO="rystudy/frps"
